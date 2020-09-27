b90ab8f97d45804b50ba7c72bc1bca8ff4c1fc94
Brian Shanken <bcshanken@gmail.com>
2020-09-26 20:46:46 -0400

    timer functionality added

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+
+// Dom Variables
 var quizStart = document.getElementById("start-quiz");
+var timeEl = document.getElementById("timer");
+var secondsLeft = 10;
 
+// functions
+
+function startTIme(){
+    var timerInterval= setInterval(function(){
+        secondsLeft--;
+        timeEl.textContent = "Time Remaining: " + secondsLeft;
+
+        if(secondsLeft === 0){
+            clearInterval(timerInterval);
+            alert("time up");
+        }
+    },1000)
+}
+
+
+
+// Doing it (I dont remember the name of this section)
 quizStart.addEventListener("click", function(){
-
+    startTIme();
     console.log("started");
 })
+
