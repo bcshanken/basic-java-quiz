022be1cba0239273c59592118d2922c970b785e4
Brian Shanken <bcshanken@gmail.com>
2020-09-29 23:26:48 -0400

    Several bugs still persist. The High score list shows up above the message, the timer from the highscore page moves at double speed, if a user hits the last question wrong the page goes blank

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 // Dom Variables
 var quizStart = document.getElementById("start-quiz");
 var timeEl = document.getElementById("timer");
-var loadScoreBoard = document.getElementById("high-scores")
+var loadScoreBoard = document.getElementById("high-scores");
 var welcomeContainer = document.getElementById("welcome");
-var quizContainer=document.getElementById("quiz");
-var YourScoreContainer=document.getElementById("highScoresSection");
+var quizContainer = document.getElementById("quiz");
+var YourScoreContainer = document.getElementById("highScoresSection");
 var scoreBoardEl = document.getElementById("scoreBoard");
 var answerOptions = document.getElementById("answers");
 var questionText = document.getElementById("question");
 var scoreBoardMessage = document.getElementById("scoresMessage");
+var scoreBoardListEl = document.getElementById("scoreBoardList");
+var scoreBoardStart = document.getElementById("start-quiz-score");
 var scoreForm = document.getElementById("score-form");
 var secondsLeft = 80;
 var currentQuestion = 0;
 var score = 20;
-var historicalScores= []
-var userInitialsInput = document.getElementById("score-initials")
-
-
-
+var historicalScores = [];
+var userInitialsInput = document.getElementById("score-initials");
 
 var quizQuestions = [
   {
@@ -67,32 +66,62 @@ var quizQuestions = [
 
 // functions
 
-function initialLoad(){
-  var localStoredScores =JSON.parse(localStorage.getItem("historicalScores"))
+function initialLoad() {
+  var localStoredScores = JSON.parse(localStorage.getItem("historicalScores"));
 
-  if (localStoredScores !== null){
-    historicalScores=localStoredScores;
+  if (localStoredScores !== null) {
+    historicalScores = localStoredScores;
   }
 }
 
 function listHighScores() {
   // code to turn other displays off
-  welcomeContainer.style.display="none";
-  quizContainer.style.display="none";
-  YourScoreContainer.style.display="none";
-  if(historicalScores.length=0){
-    var noScoreMessage = document.createElement("h3")
-    noScoreMessage.textContent="There are no scores to display, refresh to play the game"
-    scoreBoardEl.append(noScoreMessage);
-  }else{
-    console.log("scoreboard!!!")
-    var ScoreMessage = document.createElement("h3")
-    ScoreMessage.textContent="There are scores to display, refresh to play the game"
+  console.log(historicalScores.length);
+  scoreBoardEl.style.display="block";
+  welcomeContainer.style.display = "none";
+  quizContainer.style.display = "none";
+  YourScoreContainer.style.display = "none";
+  timeEl.style.display = "none";
+  // scoreboard greeting
+
+  if (historicalScores.length === 0) {
+    var ScoreMessage = document.createElement("h3");
+    ScoreMessage.textContent =
+      "There are no high scores yet, wont you try the game?";
     scoreBoardEl.append(ScoreMessage);
+  } else {
+    var ScoreMessage = document.createElement("h3");
+    ScoreMessage.textContent = "Welcome to the high Scoreboard!";
+    scoreBoardEl.append(ScoreMessage);
+    // scoreboard list
+    for (i = 0; i < historicalScores.length; i++) {
+      console.log("test");
+      var liEl = document.createElement("li");
+      liEl.textContent = historicalScores[i];
+      scoreBoardListEl.append(liEl);
+    }
+    var scorestartbutton = document.createElement("button");
+    scorestartbutton.setAttribute("class", "btn btn-danger");
+    scorestartbutton.setAttribute("id", "scoreStart");
+    scorestartbutton.textContent = "Start";
+    scoreBoardStart.append(scorestartbutton);
+    scorestartbutton.addEventListener("click", function (event) {
+      console.log("score start");
+      welcomeContainer.style.display = "none";
+      scoreBoardEl.style.display = "none";
+      quizContainer.style.display = "block";
+      timeEl.style.display="block";
+      clearInterval();
+      startTIme();
+      var answersToDisplay = quizQuestions[currentQuestion].answers;
+      renderAnswers(answersToDisplay);
+      renderQuestions();
+    });
   }
 }
 
 function startTIme() {
+  secondsLeft=80;
   var timerInterval = setInterval(function () {
     secondsLeft--;
     timeEl.textContent = "Time Remaining: " + secondsLeft;
@@ -114,49 +143,47 @@ function renderAnswers(array) {
     answerOptions.append(button);
   }
 }
-function storeScore(){
-  localStorage.setItem("historicalScores",JSON.stringify(historicalScores))
+function storeScore() {
+  localStorage.setItem("historicalScores", JSON.stringify(historicalScores));
 }
 function renderQuestions() {
   questionText.textContent = quizQuestions[currentQuestion].question;
 }
-function renderYourScore(){
-  quizContainer.style.display="none";
-  timeEl.style.display="none";
+function renderYourScore() {
+  quizContainer.style.display = "none";
+  timeEl.style.display = "none";
   var highScoreMessage = document.createElement("h2");
   var score = secondsLeft;
-  if(score<0){
-    score=0;
+  if (score < 0) {
+    score = 0;
   }
   highScoreMessage.textContent = "All Done ";
-  scoreBoardMessage.append(highScoreMessage); 
+  scoreBoardMessage.append(highScoreMessage);
   var yourScoreMessage = document.createElement("p");
-  yourScoreMessage.textContent= "Your score is " + score;
+  yourScoreMessage.textContent = "Your score is " + score;
   scoreBoardMessage.append(yourScoreMessage);
-  var formLabel =document.createElement("label");
-  formLabel.textContent="Enter your initials: ";
-  formLabel.setAttribute("id","score-form-label");
+  var formLabel = document.createElement("label");
+  formLabel.textContent = "Enter your initials: ";
+  formLabel.setAttribute("id", "score-form-label");
   scoreForm.append(formLabel);
   var initialInput = document.createElement("input");
-  initialInput.setAttribute("type","text");
-  initialInput.setAttribute("id","score-initials");
+  initialInput.setAttribute("type", "text");
+  initialInput.setAttribute("id", "score-initials");
   scoreForm.append(initialInput);
-  var button =document.createElement("button");
+  var button = document.createElement("button");
   button.setAttribute("class", "btn btn-danger");
-  button.setAttribute("id", "submit")
-  button.textContent="Submit";
+  button.setAttribute("id", "submit");
+  button.textContent = "Submit";
   scoreForm.append(button);
-  button.addEventListener("click",function(event){
+  button.addEventListener("click", function (event) {
     event.preventDefault();
-  var scoreToAddInitials = document.getElementById("score-initials");
-  var scoreToAdd = scoreToAddInitials.value.trim() + "- " + score;
-  console.log(scoreToAdd);
-  historicalScores.push(scoreToAdd);
-  storeScore();
-  })
-
-  
-
+    var scoreToAddInitials = document.getElementById("score-initials");
+    var scoreToAdd = scoreToAddInitials.value.trim() + "- " + score;
+    console.log(scoreToAdd);
+    historicalScores.push(scoreToAdd);
+    storeScore();
+    listHighScores();
+  });
 }
 
 // Doing it (I dont remember the name of this section)
@@ -164,9 +191,9 @@ initialLoad();
 
 // loading score board off of scoreboard button
 
-loadScoreBoard.addEventListener("click", function(){
-  listHighScores()
-})
+loadScoreBoard.addEventListener("click", function () {
+  listHighScores();
+});
 
 quizStart.addEventListener("click", function () {
   welcomeContainer.style.display = "none";
@@ -179,51 +206,50 @@ quizStart.addEventListener("click", function () {
 });
 
 answers.addEventListener("click", function (event) {
-  if(secondsLeft<0){
-    console.log("negative score")
+  if (secondsLeft < 0) {
+    console.log("negative score");
     renderHighScore;
-  }else{
-  if (event.target.matches("button")) {
-    var selectedanswer = event.target.getAttribute("data-value");
-    console.log(selectedanswer);
-    // selects correct answer CondA
-    if (selectedanswer === quizQuestions[currentQuestion].solution) {
-      currentQuestion++;
-      console.log("stuck in right answer loop");
-      // if on the last question CondA1
-      if (currentQuestion === quizQuestions.length) {
-        var score = secondsLeft;
-        renderYourScore();
+  } else {
+    if (event.target.matches("button")) {
+      var selectedanswer = event.target.getAttribute("data-value");
+      console.log(selectedanswer);
+      // selects correct answer CondA
+      if (selectedanswer === quizQuestions[currentQuestion].solution) {
+        currentQuestion++;
+        console.log("stuck in right answer loop");
+        // if on the last question CondA1
+        if (currentQuestion === quizQuestions.length) {
+          var score = secondsLeft;
+          renderYourScore();
+        } else {
+          // pulls next question CondA2
+          var answersToDisplay = quizQuestions[currentQuestion].answers;
+          answers.textContent = "";
+          renderAnswers(answersToDisplay);
+          renderQuestions();
+        }
       } else {
-        // pulls next question CondA2
-        var answersToDisplay = quizQuestions[currentQuestion].answers;
-        answers.textContent = "";
-        renderAnswers(answersToDisplay);
-        renderQuestions();
+        // selects wrong answer CondB
+        console.log("wrong answer submitted.");
+        currentQuestion++;
+        // is on the last question ConB1
+        if (currentQuestion === quizQuestions.length) {
+          console.log("game over" + secondsLeft);
+          var score = secondsLeft - 18;
+          console.log("score" + score);
+          renderYourScore();
+        } else {
+          // pulls next question ConB2
+          var answersToDisplay = quizQuestions[currentQuestion].answers;
+          secondsLeft = secondsLeft - 18;
+          answers.textContent = "";
+          renderAnswers(answersToDisplay);
+          renderQuestions();
+        }
       }
-    } else {
-      // selects wrong answer CondB
-    console.log("wrong answer submitted.");
-    currentQuestion++;
-    // is on the last question ConB1
-    if (currentQuestion === quizQuestions.length) {
-      console.log("game over" + secondsLeft);
-      var score = secondsLeft - 18;
-      console.log("score" + score);
-      renderYourScore();
-    } else {
-      // pulls next question ConB2
-      var answersToDisplay = quizQuestions[currentQuestion].answers;
-      secondsLeft = secondsLeft - 18;
-      answers.textContent = "";
-      renderAnswers(answersToDisplay);
-      renderQuestions();
     }
   }
-}}
 });
-
-
 
 answerOptions.addEventListener("click", function (event) {
   if (event.target.matches("button")) {
